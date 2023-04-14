30831447b8a46f812902473561a31b9cf10adcc9
Alco Based <alcoholbasednontoxic@gmail.com>
2023-04-15 02:14:59 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
-require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const Person = require('./models/person')
 const app = express()
+require('dotenv').config()
+const Person = require('./models/person')
 app.use(express.json())
-
-morgan.token('content', (req, res) => JSON.stringify(req.body))
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(express.static('build'))
-
-
-let persons = []
 
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
     res.json(persons)
   })
-})
-
-app.get('/info', (req, res) => {
-  res.send(`
-  <p>Phonebook has info for ${Person.estimatedDocumentCount()} people</p>
-  <p>${new Date}</p>
-  `)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  
-  res.status(204).end()
 })
 
 app.post('/api/persons', (req, res) => {
