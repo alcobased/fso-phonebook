1dc744e594fc2ffbaed57bc1b820ff89f7ed5753
Alco Based <alcoholbasednontoxic@gmail.com>
2023-04-16 21:59:13 +0300

    uibuild

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use(express.static('build'))
 
 app.get('/api/persons', (req, res) => {
+  console.log('api call for all persons')
   Person.find({}).then(persons => {
     res.json(persons)
   })
@@ -22,5 +23,21 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+})
+
+app.get('/api/persons/:id', (req,res) => {
+  console.log(`api call to find by id: ${req.params.id}`)
+  Person.findById(req.params.id)
+    .then(result => {
+      res.json(result)
+    })
+})
+
 const PORT = process.env.PORT
 app.listen(PORT)
+console.log(`listening on port ${PORT}`)
